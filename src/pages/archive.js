dc3309e9002f4bd966aa1d7074f3420fed915a5e
Yonas <yone920@gmail.com>
2019-12-01 22:34:09 -0500

    post archive graphql query configured

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -2,11 +2,44 @@ import React from 'react'
 import Layout from '../components/layout'
 import { StaticQuery, graphql } from 'gatsby'
 
+
+const POST_ARCHIVE_QUERY = graphql`
+  query BlogPostArchive {
+    allMarkdownRemark(limit: 5, sort: {
+    order: DESC,
+    fields: [frontmatter___date]
+  }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+
 export default function archive() {
-    return (
-        <Layout>
+    <StaticQuery 
+        query = {POST_ARCHIVE_QUERY}
+        render={({allMarkdownRemark}) => (
+            <>
+                <aside>
+                    <h3>Archive</h3>
+                    <ul>
+                        {allMarkdownRemark.edge.map(edge => (
+                            <li key={edge.node.frontmatter.slug}>
+                                {edge.node.frontmatter.title}
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
+            </>
+        )}
+        />
 
+        }
 
-        </Layout>
-    )
-}

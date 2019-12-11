28412103e6da46b9e7731177401f358cd1fd7ca4
Yonas <yone920@gmail.com>
2019-12-11 15:24:03 -0500

    Gatsby netlify plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,7 @@ module.exports = {
         useACF: true,
       },
     },
+    `gatsby-plugin-netlify`,
     
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline

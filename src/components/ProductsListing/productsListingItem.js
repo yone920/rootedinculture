f2fe18cd07a40f029a72ddb0d47dd4cd6fdf1aa3
Yonas <yone920@gmail.com>
2019-12-16 16:13:02 -0500

    Netlify deploy bug fix

--- src/components/ProductsListing/productsListingItem.js
+++ src/components/ProductsListing/productsListingItem.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Image from "gatsby-image"
-import AddToCart from "../Cart/AddToCart"
+import AddToCart from "../Cart/addToCart"
 
 const ProductsListingItem = ({ product }) => {
   const {
